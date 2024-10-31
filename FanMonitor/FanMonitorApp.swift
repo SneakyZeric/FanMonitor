7b43bf3c5a6d493c11d6c6d27ce7a36ce8932fee
SneakyZeric <zericmain@gmail.com>
2024-10-31 15:54:56 -0500

    Initial Commit

---
+++ FanMonitor/FanMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FanMonitorApp.swift
+//  FanMonitor
+//
+//  Created by Eric Li on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FanMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
